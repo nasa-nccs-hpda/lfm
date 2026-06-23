d43d2dc56f50f4c9330b5099649417e2fb35afc9
sandykerr <sandykerr20@gmail.com>
2026-06-23 14:28:42 -0400

    adding container support for H100 sessions

---
+++ copy_kernel.sh
@@ -0,0 +1,5 @@
+echo "Copying kernel info..."
+KERNEL_PATH=~/.local/share/jupyter/kernels/lfm
+mkdir -p $KERNEL_PATH
+cp -r /panfs/ccds02/nobackup/projects/lfm/lfm_kernel.json $KERNEL_PATH
+echo "Done! Kernel should appear in JupyterHub as \"lfm_kernel\"."
