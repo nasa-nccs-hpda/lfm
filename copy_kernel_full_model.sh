07b11ba635801fb33ea5728f4b6a2f3ff2b04048
sandykerr <sandykerr20@gmail.com>
2026-07-31 13:25:48 -0400

    updating notebooks to work with new kernel setup

--- copy_kernel_full_model.sh
+++ copy_kernel_full_model.sh
@@ -1,6 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 echo "Copying kernel info..."
 module load miniforge
 mamba activate /explore/nobackup/projects/lfm/lfm-full-env
+export PYTHONNOUSERSITE=1
+python -m pip install -e .
 python -m ipykernel install --user --name lfm-full-env --display-name "lfm-full-env"
 mamba deactivate
 module purge
