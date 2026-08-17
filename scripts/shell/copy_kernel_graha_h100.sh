f366850156f6176ebd428ff1ad65e872947341f1
sandykerr <sandykerr20@gmail.com>
2026-08-17 12:38:13 -0400

    moving kernel scripts

--- scripts/shell/copy_kernel_graha_h100.sh
+++ scripts/shell/copy_kernel_graha_h100.sh
@@ -1,6 +1,7 @@
-echo "Copying kernel info..."
+echo "Copying kernel info and clearing old kernels..."
+rm -rf ~/.local/lib/python*
 rm -rf ~/.local/share/jupyter/kernels/*
 KERNEL_PATH=~/.local/share/jupyter/kernels/lfm
 mkdir -p $KERNEL_PATH
 cp -r /panfs/ccds02/nobackup/projects/lfm/containers/kernel.json $KERNEL_PATH
-echo "Done! Kernel should appear in JupyterHub as \"lfm_kernel_h100\"."
+echo "Done! Kernel should appear in JupyterHub as \"lfm_kernel\"."
