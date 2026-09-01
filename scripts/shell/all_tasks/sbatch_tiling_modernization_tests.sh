8958d3601076dd8eb4281173945a87ab138baafb
Sandy Kerr <sandykerr20@gmail.com>
2026-09-01 10:41:48 -0400

    fixing partition for test

--- scripts/shell/all_tasks/sbatch_tiling_modernization_tests.sh
+++ scripts/shell/all_tasks/sbatch_tiling_modernization_tests.sh
@@ -26,7 +26,7 @@ else
 fi
 
 REPO_PARENT="$(dirname "${REPO_DIR}")"
-CONTAINER_PATH="${CONTAINER_PATH:-/explore/nobackup/projects/lfm/containers/lfm-container}"
+CONTAINER_PATH="${CONTAINER_PATH:-/explore/nobackup/projects/lfm/containers/lfm-container-ipyleaflet}"
 APPTAINER_BIN="${APPTAINER_BIN:-apptainer}"
 APPTAINER_BIND_PATHS="${APPTAINER_BIND_PATHS:-/panfs/ccds02/nobackup:/explore/nobackup}"
 
